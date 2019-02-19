969275dbe0f7b65c9087b08950029687dc8936bc
patricspires <patric.pires@outlook.com>
2019-02-19 17:35:01 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo apt-get install zsh
-cp .zshrc ~/.zshrc
+clear
+echo "Initializing...\n"
+echo "Hello, $(whoami)!\n Long time we don't talk\n"
 
-
-# install nvm
-sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
-
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm --version
-
-# install vs code
-sudo apt-get install code
-
-# install vscode extensions
-code --install-extension naumovs.color-highlight
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dbaeumer.jshint
-code --install-extension guillaumedoutriaux.name-that-color
-code --install-extension michelemelluso.code-beautifier
-code --install-extension zignd.html-css-class-completion
-code --install-extension ritwickdey.liveserver
-code --install-extension shinnn.stylelint
-code --install-extension calvinhong.stylelint-fix
-code --install-extension eamodio.gitlens
-code --install-extension pnp.polacode
-code --install-extension 2gua.rainbow-brackets
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension ionutvmi.path-autocomplete
-code --install-extension equinusocio.vsc-material-theme
-code --install-extension pkief.material-icon-theme
-code --install-extension oderwat.indent-rainbow
-code --install-extension hwencc.html-tag-wrapper
+sh $HOME/workspace/.dotfiles/install/linux.sh
