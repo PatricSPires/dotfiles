2c068fc9b68e2e5d0871b43439dc03da98753da4
patricspires <patric.pires@outlook.com>
2019-02-19 17:42:55 -0300

    update vscode config

--- install/vscode.sh
+++ install/vscode.sh
@@ -4,13 +4,11 @@ sudo apt-get install code
 # install vscode extensions
 code --install-extension naumovs.color-highlight
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension dbaeumer.jshint
 code --install-extension guillaumedoutriaux.name-that-color
 code --install-extension michelemelluso.code-beautifier
 code --install-extension zignd.html-css-class-completion
 code --install-extension ritwickdey.liveserver
 code --install-extension shinnn.stylelint
-code --install-extension calvinhong.stylelint-fix
 code --install-extension eamodio.gitlens
 code --install-extension pnp.polacode
 code --install-extension 2gua.rainbow-brackets
@@ -20,3 +18,4 @@ code --install-extension equinusocio.vsc-material-theme
 code --install-extension pkief.material-icon-theme
 code --install-extension oderwat.indent-rainbow
 code --install-extension hwencc.html-tag-wrapper
+code --install-extension deerawan.vscode-dash
