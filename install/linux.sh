8787052ccb22f1a8d7d1bdb4f5a667fe0f6335e7
patricspires <patric.pires@outlook.com>
2019-04-10 22:58:30 -0300

    extract zsh installation

--- install/linux.sh
+++ install/linux.sh
@@ -51,8 +51,4 @@ sh ./vscode.sh
 # install zeal
 sudo apt-get install zeal
 
-# install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo apt-get install zsh
-chsh -s /bin/zsh
-cp .zshrc ~/.zshrc
+sh ./zsh.sh
---
+++ install/zsh.sh
@@ -0,0 +1,5 @@
+# install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install zsh
+chsh -s /bin/zsh
+cp .zshrc ~/.zshrc
