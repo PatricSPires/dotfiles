1cab697d277d345a9b2ffe36ad536cd1cfb34f22
PatricSPires <patric.pires@outlook.com>
2020-04-10 22:50:46 -0300

    feat(vscode): add some informations and move folder

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,5 +1,4 @@
 // Vscode extensions:
-code --install-extension ahmadawais.shades-of-purple
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
@@ -7,7 +6,10 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension jpoissonnier.vscode-styled-components
-code --install-extension mquandalle.graphql
 code --install-extension naumovs.color-highlight
 code --install-extension PKief.material-icon-theme
-code --install-extension wix.vscode-import-cost
+code --install-extension rocketseat.RocketseatReactJS
+code --install-extension zhuangtongfa.material-theme
+
+#This command will list all your code extensions
+##code --list-extensions | xargs -L 1 echo code --install-extension 
