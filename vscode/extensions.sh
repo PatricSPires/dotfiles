f06186b654e31154acb2a92d07754c330ef8d5a9
patricSPires <patric.pires@outlook.com>
2020-05-03 00:21:06 -0300

    feat(vscode): add more extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -4,11 +4,20 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension mikestead.dotenv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-audio
 code --install-extension naumovs.color-highlight
 code --install-extension PKief.material-icon-theme
-code --install-extension rocketseat.RocketseatReactJS
+code --install-extension Prisma.vscode-graphql
+code --install-extension rocketseat.rocketseatreactjs
+code --install-extension stylelint.vscode-stylelint
+code --install-extension WakaTime.vscode-wakatime
 code --install-extension zhuangtongfa.material-theme
 
 #This command will list all your code extensions
